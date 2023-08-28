8f6c170a5eba1cba9f500fdcc9a2cf313d64e98f
Julian22222 <julik.golovenj@gmail.com>
2023-08-28 20:22:19 +0100

    rps

--- src/components/RPS.jsx
+++ src/components/RPS.jsx
@@ -33,7 +33,7 @@ function RPS() {
   // block to choose RemotePlayback, paper,scissors
   const [showMainBlock, setShowMainBlock] = useState(true);
 
-  //chousen your rock,paper,scissors
+  //selected rock,paper or scissors
   const [yourValue, setYourValue] = useState(null);
   const [showOpponent, setShowOpponent] = useState(null);
 
@@ -43,9 +43,6 @@ function RPS() {
   //shaking hands ,pre fight
   const [preFight, setPreFight] = useState(false);
 
-  //   play next round btn
-  //   const [showNextRound, setShowNextRound] = useState(false);
-
   //points for myself and opponent
   const [myPoints, setMyPoints] = useState(0);
   const [opponentPoints, setOpponentPoints] = useState(0);
@@ -54,13 +51,16 @@ function RPS() {
   const [showClickMe, setShowClickMe] = useState(true);
 
   //show win sign or lose sign
-  const [showWin, setShowWin] = useState(null);
-  const [showLose, setShowLose] = useState(null);
-  const [showDraw, setShowDraw] = useState(null);
+  const [showWin, setShowWin] = useState(false);
+  const [showLose, setShowLose] = useState(false);
+  const [showDraw, setShowDraw] = useState(false);
 
   //end msg you won ,CPU wins
   //   you lost the game ,let's try again!
   const [showEndMsg, setShowEndMsg] = useState(false);
+
+  //game progress
+  const [gameProgress, setGameProgress] = useState(0);
 
   const opponent = [
     { logo: "../IMG/1.png", num: 1, title: "rock" },
@@ -71,6 +71,8 @@ function RPS() {
   ////////////////////////////////////////////////////////////////////////////////////////vvvvv/////useREffect
 
   useEffect(() => {
+    console.log("second");
+    if (!yourValue || !showOpponent) return;
     // YOUR_CODE_HERE WILL RUN WHEN the -ourValue is change
     // const myRandom = Math.floor(Math.random() * 3); //random number from 0 - 2
     // setTimeout(setShowOpponent(opponent[myRandom]), 2000);
@@ -78,37 +80,49 @@ function RPS() {
     // console.log("yourValue", yourValue);
     // console.log("myValue - useEffect block", yourValue?.num);
     // console.log("oppValue useEffect block", showOpponent?.num);
-
     // add points
+
+    ///////////////////////////////////////////////////////// adding points
+    //////////////////////////////////////////// show win sign or lose sign
+    /////////////////////////////////////////////////////////show win/lose logo
     setTimeout(() => {
       if (yourValue?.num === 1 && showOpponent?.num === 2) {
-        //     setOpponentPoints(opponentPoints + 1);
+        setOpponentPoints(opponentPoints + 1);
         setShowLose(true);
+        loseTrack();
       }
 
       if (yourValue?.num === 1 && showOpponent?.num === 3) {
-        //     setMyPoints(myPoints + 1);
+        setMyPoints(myPoints + 1);
+        console.log(myPoints, "help");
         setShowWin(true);
+        winTrack();
       }
 
       if (yourValue?.num === 2 && showOpponent?.num === 1) {
-        //     setMyPoints(myPoints + 1);
+        setMyPoints(myPoints + 1);
+        console.log(myPoints, "help");
         setShowWin(true);
+        winTrack();
       }
 
       if (yourValue?.num === 2 && showOpponent?.num === 3) {
-        //     setOpponentPoints(opponentPoints + 1);
+        setOpponentPoints(opponentPoints + 1);
         setShowLose(true);
+        loseTrack();
       }
 
       if (yourValue?.num === 3 && showOpponent?.num === 1) {
-        //     setOpponentPoints(opponentPoints + 1);
+        setOpponentPoints(opponentPoints + 1);
         setShowLose(true);
+        loseTrack();
       }
 
       if (yourValue?.num === 3 && showOpponent?.num === 2) {
-        //     setMyPoints(myPoints + 1);
+        setMyPoints(myPoints + 1);
+        console.log(myPoints, "help");
         setShowWin(true);
+        winTrack();
       }
 
       if (
@@ -117,87 +131,45 @@ function RPS() {
         (yourValue?.num === 3 && showOpponent?.num === 3)
       ) {
         setShowDraw(true);
-      }
-
-      //   console.log("myPoints", myPoints);
-      //   console.log("opponentPoints", opponentPoints);
-
-      if (myPoints === 3) {
-        setShowEndMsg("GAME OVER. YOU WON THE GAME!!!! ,GREAT JOB!!!! ");
-        setShowDraw(false);
-        setShowWin(false);
-        setShowLose(false);
-        setShowMainBlock(false);
-        setShowClickMe(false);
-      }
-
-      if (opponentPoints === 3) {
-        setShowEndMsg("GAME OVER.  YOU LOST THE GAME ,LET'S TRY AGAIN! ");
-        setShowDraw(false);
-        setShowWin(false);
-        setShowLose(false);
-        setShowMainBlock(false);
-        setShowClickMe(false);
-      }
-    }, 1100);
-
-    //////////////////////////////////////////////// show win sign or lose sign
-    setTimeout(() => {
-      if (yourValue?.num === 1 && showOpponent?.num === 2) {
-        // setShowLose(true);
-        loseTrack();
-      }
-
-      if (yourValue?.num === 1 && showOpponent?.num === 3) {
-        // setShowWin(true);
-        winTrack();
-      }
-
-      if (yourValue?.num === 2 && showOpponent?.num === 1) {
-        // setShowWin(true);
-        winTrack();
-      }
-
-      if (yourValue?.num === 2 && showOpponent?.num === 3) {
-        // setShowLose(true);
-        loseTrack();
-      }
-
-      if (yourValue?.num === 3 && showOpponent?.num === 1) {
-        // setShowLose(true);
-        loseTrack();
-      }
-
-      if (yourValue?.num === 3 && showOpponent?.num === 2) {
-        // setShowWin(true);
-        winTrack();
-      }
-
-      if (
-        (yourValue?.num === 1 && showOpponent?.num === 1) ||
-        (yourValue?.num === 2 && showOpponent?.num === 2) ||
-        (yourValue?.num === 3 && showOpponent?.num === 3)
-      ) {
-        // setShowDraw(true);
         drawTrack();
       }
-    }, 900);
+    }, 100);
 
-    // if (myPoints === 3) {
-    //   setShowEndMsg("YOU WON THE GAME!!!! ,GREAT JOB!!!! ");
-    // }
+    /////////////////////////////////////////////////////////
 
-    // if (showOpponent === 3) {
-    //   setShowEndMsg("YOU LOST THE GAME ,LET'S TRY AGAIN! ");
-    // }
-  }, [yourValue, showOpponent, showEndMsg, myPoints, opponentPoints]);
+    console.log("myPoints", myPoints);
+    console.log("opponentPoints", opponentPoints);
 
+    setGameProgress(1);
+  }, [gameProgress]);
   /////////////////////////////////////////////////////////////////////////////////////////////////////////useEffect
 
+  /////////////////////////////////////////////////////////////////////////vvvvvvvvvvvvvvvvv////////second useEffect
+  useEffect(() => {
+    if (myPoints === 3) {
+      setShowEndMsg("GAME OVER. YOU WON THE GAME!!!! ,GREAT JOB!!!! ");
+      setShowDraw(false);
+      setShowWin(false);
+      setShowLose(false);
+      setShowMainBlock(false);
+      setShowClickMe(false);
+    }
+
+    if (opponentPoints === 3) {
+      setShowEndMsg("GAME OVER. YOU LOST THE GAME ,LET'S TRY AGAIN! ");
+      setShowDraw(false);
+      setShowWin(false);
+      setShowLose(false);
+      setShowMainBlock(false);
+      setShowClickMe(false);
+    }
+  }, [myPoints, opponentPoints]);
+
+  console.log(gameProgress);
   console.log("myPoints outside - ", myPoints);
   console.log("opponentPoints outside - ", opponentPoints);
 
-  //////////////////////////////////////////////////////////music effects
+  //////////////////////////////////////////////////////////////////////music effects
 
   const playShake = () => {
     new Audio(shakeSound).play();
@@ -223,32 +195,32 @@ function RPS() {
     new Audio(drawSound).play();
   };
 
-  ///////////////////////////////////////////////////handle Info btn
+  ///////////////////////////////////////////////handle Info btn
   const handleInfo = () => {
     setShowInfo(true);
   };
 
-  ///////////////////////////////////////////////////handle close Info btn
+  //////////////////////////////////////////////handle close Info btn
   const handleCloseInfo = () => {
     setShowInfo(false);
   };
 
-  ////////////////////////////////////////////////handle Play Again btn
+  /////////////////////////////////////////////////handle Play Again btn
   const handlePlayAgain = () => {
     setShowClickMe(true);
     setShowMainBlock(true);
     setMyPoints(0);
     setOpponentPoints(0);
     setShowEndMsg(false);
-    setShowLose(null);
-    setShowWin(null);
-    setShowDraw(null);
-    setYourValue(null);
-    setShowOpponent(null);
+    setShowLose(false);
+    setShowWin(false);
+    setShowDraw(false);
+    setYourValue(false);
+    setShowOpponent(false);
     setShowBattle(true);
   };
 
-  /////////////////////////////////////////////////////handleClick
+  /////////////////////////////////////////////////////////////////////////////////handleClick
 
   const handleClick = (e) => {
     playShake();
@@ -256,17 +228,17 @@ function RPS() {
     setShowWin(false);
     setShowLose(false);
     setShowClickMe(false);
-    setYourValue(null);
-    setShowOpponent(null);
+    setYourValue(false);
+    setShowOpponent(false);
     //remove epty battle field(red/blue table)
     setShowBattle(false);
 
     setShowMainBlock(false);
 
-    // show shaking hands
+    ///////////////////// show shaking hands
     setPreFight(true);
 
-    //hide shaking hands
+    //////////////////////hide shaking hands
     setTimeout(() => {
       setPreFight(false);
     }, 1000);
@@ -275,13 +247,13 @@ function RPS() {
       playHit();
     }, 1000);
 
-    // set your choise
+    //////////////////////////// set your choise
     setTimeout(() => {
       setYourValue(opponent[e - 1]);
     }, 1000);
 
     const myRandom = Math.floor(Math.random() * 3); //random number from 0 - 2
-    //set opponent choise
+    //////////////////////////////////////////////////set opponent choise
     setTimeout(() => {
       setShowOpponent(opponent[myRandom]);
     }, 1000);
@@ -292,9 +264,9 @@ function RPS() {
       setShowDraw(false);
       setShowClickMe(true);
       setShowMainBlock(true);
-      // setYourValue(null);
-      // setShowOpponent(null);
-      //   setShowNextRound(false);
+
+      setGameProgress(2);
+      console.log("end of handle click");
     }, 2000);
 
     // console.log(e);
@@ -302,49 +274,10 @@ function RPS() {
     // console.log("yourValue", yourValue);
     // console.log("Random", myRandom);
     // console.log("Random element", opponent[myRandom]);
-
-    setTimeout(() => {
-      if (yourValue?.num === 1 && showOpponent?.num === 2) {
-        setOpponentPoints(opponentPoints + 1);
-        // setShowLose(true);
-      }
-
-      if (yourValue?.num === 1 && showOpponent?.num === 3) {
-        setMyPoints(myPoints + 1);
-        // setShowWin(true);
-      }
-
-      if (yourValue?.num === 2 && showOpponent?.num === 1) {
-        setMyPoints(myPoints + 1);
-        // setShowWin(true);
-      }
-
-      if (yourValue?.num === 2 && showOpponent?.num === 3) {
-        setOpponentPoints(opponentPoints + 1);
-        // setShowLose(true);
-      }
-
-      if (yourValue?.num === 3 && showOpponent?.num === 1) {
-        setOpponentPoints(opponentPoints + 1);
-        // setShowLose(true);
-      }
-
-      if (yourValue?.num === 3 && showOpponent?.num === 2) {
-        setMyPoints(myPoints + 1);
-        // setShowWin(true);
-      }
-    }, 1000);
   };
 
   return (
     <div class="all-page-rps">
-      {/* <Sound
-        url={track}
-        playStatus={Sound.status.PLAYING}
-        playFromPosition={300}
-      /> */}
-
-      {/* <AudioPlayer src={track} onPlay={(e) => console.log(e)} /> */}
       <img
         // width={150}
         // height={100}
@@ -363,20 +296,10 @@ function RPS() {
             alt="info picture"
           />
         </button>
-        {/* <button className="music-btn">
-         
-          <img
-            width={60}
-            height={30}
-            src={music}
-            className="music"
-            alt="music picture"
-          />
-        </button> */}
       </div>
       <div className="container">
         <div className="flexbox-leftside">
-          {/* /////////click me sign */}
+          {/* //////////////////click me sign */}
           {showClickMe ? (
             <img
               class
@@ -512,7 +435,7 @@ function RPS() {
             )}
           </div>
 
-          {/* ///////////////////////////////////////////////////////////////////////batle */}
+          {/* ///////////////////////////////////////////////////////////////////////battle */}
 
           {/* //////////////////////////////////////////////////////////////////////////my element */}
           <div className={yourValue ? "battle-field-change" : "battle-field"}>
